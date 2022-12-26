2df93a39ae5b0c576e4f128c24121f4e5ffa23ca
0xalankrit <alankritnokhwal@gmail.com>
2022-12-27 02:54:11 +0530

    Burnable extension added

--- contracts/Astronauts.sol
+++ contracts/Astronauts.sol
@@ -9,101 +9,76 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
+
+// ---------------------------------
+// IMPORTANT                        
+// 0 -change the constructor.
+// 1- Change in base ERC721 contract
+// 2- Change in counter library     
+// 3- Reignlabs wallet to be replaced with the MultiSig wallet
+// 4- ERC721 base contract updated wont work on remix
+// ---------------------------------                    
+
 contract ERC721Holder is IERC721Receiver {
   function onERC721Received(address, address, uint256, bytes memory) public pure returns(bytes4){
     return this.onERC721Received.selector;
   }
 }   
-// TODO:
-
-// DISCUSS :                                                  
-// FINAL DISCUSS-                                             
-// DONE free mint is per wallet not per NFT                  
-// what if holders transfer their NFTs ???????????????????    
-
-
-// ERC721 base contract updated wont work on remix
-// Update PAUSE FUNCTION
-
-// Make it pausable
-// MintDiscountedInvestorNFTs require(amount<20);
-
-// GOAL:
-// only signature that comes from the database should be used
-// timestamp on signature to when it is available
-
-// user mint then stake 
-// t0 ->staked, availabe to claim -0
-// t1 ->avalible to claim -$7 pass the signature in claim function claimed, signature can not be used
-// update claimed till now  
-
-// ---------------------------------
-// IMPORTANT                        
-// 1- Change in base ERC721 contract
-// 2- Change in counter library     
-// 3- Reignlabs wallet to be replaced with the MultiSig wallet
-// --------------------------------- 
 
 interface NFTContract {     
     function balanceOf(address owner) external view returns (uint256 balance);
 }                            
 
 contract Astronauts is ERC721, IERC721Receiver,ERC721Holder{
-    using Counters for Counters.Counter;        
+    using Counters for Counters.Counter;                            
         
     Counters.Counter private _tokenIdCounter;   
-    uint256 public NFTprice;    
-    uint256 public DiscountedNFTprice;  
-    uint256 public NFTcap;          
-    address public ReignLabsWallet;     
-    address public MultiSigWallet;      
-    mapping(address=>bool) public validator;    
-    address public MooseSociety;    
-    address public AlphaHeard;      
-    
+    uint256 public NFTprice;    // 
+    uint256 public DiscountedNFTprice;
+
+    uint32 public NFTcap;      
+    uint32 public constant MooseCap =100;         
+    uint32 public constant AlphaCap =32;          
+    uint32 public MooseCount;                     
+    uint32 public AlphaCount;    
+
     bool public discountPaused; 
     bool public contractPaused;  
 
-    address[] public NFTProjects;               
+    address public MultiSigWallet;      
+    address public MooseSociety;    
+    address public AlphaHeard;       
                                                 
+    address[] public NFTProjects;               
+    uint256[] private _allTokens;                       
+    mapping(address=>bool) public validator;    
     mapping(address=>bool) public claimedMoose; 
     mapping(address=>bool) public claimedAlpha; 
-    uint public constant MooseCap =100;         
-    uint public constant AlphaCap =30;          
-    uint public MooseCount;                     
-    uint public AlphaCount;                      
+    mapping(address => mapping(uint256 =>bool)) public StakedNFT;
+    mapping(address => uint256) public claimedSoFar;
+    mapping(address => mapping(uint256 => uint256)) private _ownedTokens;
+    mapping(uint256 => uint256) private _ownedTokensIndex;
+    mapping(uint256 => uint256) private _allTokensIndex;
+
     string public baseURI = "https://reignkit.reignlabs.io/api/nauts/"; 
 
     // struct StakedMetaData{   
     //     address owner;       
     //     uint timeStamp;      
     // }                         
-
-    // multiple staking and multiple unstaking tokenId;
-    // Listing of minted nft per wallet         
-    // list of staked nfts per wallet   
-
-    mapping(address => mapping(uint256 =>bool)) public StakedNFT;
-    mapping(address => uint256) public claimedSoFar;
     // mapping(uint=>StakedMetaData) public StakedNFTbyId; 
-    // uint dispersalCounter;                           
-    mapping(address => mapping(uint256 => uint256)) private _ownedTokens;
-    mapping(uint256 => uint256) private _ownedTokensIndex;
-    uint256[] private _allTokens;                       
-    mapping(uint256 => uint256) private _allTokensIndex;
-
-    constructor() ERC721("Astronauts", "ASTRO")
-    payable
-    {
+                           
+    // constructor(address a,address b) ERC721("Astronauts", "ASTRO") payable{
+    constructor() ERC721("Astronauts", "ASTRO") payable{
         MooseSociety=0xC0485b2005a6840180937A7cc6b89BBed2281b94; // Moose Society address
         AlphaHeard=0x91133E3BB20a9183eED2c9cf8DaD28D2d268BACb;   // Alpha Island address
                                                                                 
         MultiSigWallet=0x80dCC025a1A8D821e87a310d57feD12A18C25F00;  // To transfer the funds from this wallet
-                                                                                
+                                                                    
         validator[msg.sender]=true;         
         NFTprice =150;                
         DiscountedNFTprice =110;      
-        NFTcap=1969;                  
+        NFTcap=1970;                  
         NFTProjects.push(0xC0485b2005a6840180937A7cc6b89BBed2281b94); // Moose Society address
         NFTProjects.push(0x91133E3BB20a9183eED2c9cf8DaD28D2d268BACb); // Alpha Island address
         NFTProjects.push(0x6bf946A39701f8A2eb5F33e31F4D092eb8ed90D9); // Grouchy tigers     
@@ -121,16 +96,9 @@ contract Astronauts is ERC721, IERC721Receiver,ERC721Holder{
         _;
     }
 
-    // CAUTION: Don't use this in production
-    function addFreeMintAddresses(address mooseSociety,address alphaHeard) public onlyValidator{
-        MooseSociety=mooseSociety;
-        AlphaHeard=alphaHeard;
-    }
-
-    // Tested 
-    function claimFreeInvestorNFTs() public whenNotPaused{
-        require(MooseCount<MooseCap);
-        require((AlphaCount+1)<AlphaCap);
+    function claimFreeInvestorNFTs() public whenNotPaused {
+        require(MooseCount<MooseCap,"Moose cap reached");
+        require((AlphaCount+1)<AlphaCap,"Alpha cap reached");
         uint256 tokenId = _tokenIdCounter.current();
         uint amount;                                 
         if((claimedMoose[msg.sender]==false && NFTContract(MooseSociety).balanceOf(msg.sender)>0) && (claimedAlpha[msg.sender]==false && NFTContract(AlphaHeard).balanceOf(msg.sender)>0)){
@@ -150,8 +118,6 @@ contract Astronauts is ERC721, IERC721Receiver,ERC721Holder{
         }else{
             revert("Can't claim free Investors NFTs");
         }
-        // Won't mint if cap is after 2 mints and you have 3 claims
-        // If I calim and transfer my moose to some other address he can also claim and transfer me back.
         require(tokenId+amount<=NFTcap,"Maximum cap reached for asto-nauts NFTs");
         for(uint i=0; i<amount; i++){
             uint256 tokenIdlocal = _tokenIdCounter.current();
@@ -160,7 +126,6 @@ contract Astronauts is ERC721, IERC721Receiver,ERC721Holder{
         }   
     }   
 
-    // Tested
     function MintInvestorNFTs(address to, uint amount) public whenNotPaused payable{
         uint256 tokenId = _tokenIdCounter.current();
         require(tokenId+amount<=NFTcap,"Maximum cap reached for asto-nauts NFTs");
@@ -175,20 +140,19 @@ contract Astronauts is ERC721, IERC721Receiver,ERC721Holder{
             _safeMint(to, tokenIdlocal);
         }
     }   
-
-    // Tested   
+  
     function MintDiscountedInvestorNFTs(address to, uint amount) public whenNotPaused payable {
         require(discountPaused==false,"Discounted mint function is currently paused");
-        require(amount<=50);
-        bool isHolder;
-        uint256 tokenId = _tokenIdCounter.current(); 
-        for(uint i=0; i<NFTProjects.length; i++){
-            if(NFTProjects[i]== address(0)) continue;
+        // require(amount<=50,"Please pass a value less then 50");
+        bool isHolder;                  
+        uint256 tokenId = _tokenIdCounter.current();    
+        for(uint i=0; i<NFTProjects.length; i++){   
+            if(NFTProjects[i]== address(0)) continue;   
             if(NFTContract(NFTProjects[i]).balanceOf(msg.sender)>0){
-                isHolder=true;
-                break;
-            }
-        }       
+                isHolder=true;          
+                break;          
+            }           
+        }           
         require(isHolder==true,"Caller can't mint discounted NFTs");
         require(msg.value>= DiscountedNFTprice*amount ,"Please send sufficient amount of ethers");
         
@@ -205,7 +169,7 @@ contract Astronauts is ERC721, IERC721Receiver,ERC721Holder{
 
     function stakeInvestorNFT(uint[] memory tokenIDs) public whenNotPaused {
         for(uint i=0; i<tokenIDs.length; i++){
-            require(StakedNFT[msg.sender][tokenIDs[i]]==false);
+            require(StakedNFT[msg.sender][tokenIDs[i]]==false,"Already Staked");
             require(ownerOf(tokenIDs[i])==msg.sender,"Not the Owner of this tokenID");
             // safeTransferFrom(msg.sender,address(this),tokenIDs[i]);
             StakedNFT[msg.sender][tokenIDs[i]]=true;
@@ -222,13 +186,11 @@ contract Astronauts is ERC721, IERC721Receiver,ERC721Holder{
         }
     }     
 
-    // Tested 
     function addValidator(address _address) public onlyValidator{
         require(validator[_address]==false,"Already a validator");
         validator[_address]=true;
     }   
 
-    // Tested 
     function addNFTProject(address _address) public onlyValidator{
         require(_address!=address(0),"Invalid address");
         for(uint i=0; i<NFTProjects.length; i++){
@@ -237,7 +199,6 @@ contract Astronauts is ERC721, IERC721Receiver,ERC721Holder{
         NFTProjects.push(_address);           
     } 
 
-    // Tested 
     function removeNFTProject(address _address) public onlyValidator{
         for(uint i=0;i<NFTProjects.length; i++){
             if(NFTProjects[i]==_address) { 
@@ -247,13 +208,17 @@ contract Astronauts is ERC721, IERC721Receiver,ERC721Holder{
         }
     }
 
-    // Tested 
     function removeValidator(address _address) public onlyValidator{
         require(validator[msg.sender]==true,"Not a validator");
         validator[_address]=false;
     }               
 
-    // Tested 
+    function burn(uint256 tokenId) public virtual {
+        //solhint-disable-next-line max-line-length
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner or approved");
+        _burn(tokenId);
+    }
+
     function contractBalance() public view returns (uint256) {
         return address(this).balance;
     }       
@@ -262,13 +227,11 @@ contract Astronauts is ERC721, IERC721Receiver,ERC721Holder{
         return block.timestamp;
     }        
 
-    // Tested 
     function withdrawEthers(uint amount) public onlyValidator {
         (bool success, ) = (MultiSigWallet).call{value: amount}("");
         require(success, "Failed to send ethers");
-    }          
+    }
 
-    // Tested 
     function updtateNFTPice(bool discounted, uint price) public onlyValidator{
         if(discounted){
             DiscountedNFTprice=price;
@@ -277,7 +240,6 @@ contract Astronauts is ERC721, IERC721Receiver,ERC721Holder{
         }
     }   
 
-    // Tested 
     function pauseUnpauseDiscountMint(bool pause) public onlyValidator{
         discountPaused=pause;
     }
@@ -298,8 +260,12 @@ contract Astronauts is ERC721, IERC721Receiver,ERC721Holder{
         return _ownedTokens[owner][index];
     }
 
+    // function totalSupply() public view virtual returns (uint256) {
+    //     return (_tokenIdCounter.current()-1);
+    // }   
+    
     function totalSupply() public view virtual returns (uint256) {
-        return (_tokenIdCounter.current()-1);
+        return _allTokens.length;
     }
 
     function _baseURI() internal view override returns (string memory) {
